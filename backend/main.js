f8c58d3c6bbbbc24ce70c40093e69b043281d6ec
Fakhrizal Mustaqim <fakhrizaluciha@gmail.com>
2025-06-04 08:48:05 +0700

    fix port

--- backend/main.js
+++ backend/main.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 const cors = require('cors');
 const bp = require("body-parser");
-const PORT = process.env.PORT || 8000
+const PORT = process.env.PORT
 const router = require("./routes/index")
 
 app.use(cors());
