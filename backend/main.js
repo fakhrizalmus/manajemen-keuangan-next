1816e076c6cd1a9bc57367a50faba2906806bc62
Fakhrizal Mustaqim <fakhrizaluciha@gmail.com>
2025-06-23 10:37:37 +0700

    add api docs

--- backend/main.js
+++ backend/main.js
@@ -5,11 +5,14 @@ const cors = require('cors');
 const bp = require("body-parser");
 const PORT = process.env.PORT
 const router = require("./routes/index")
+const swaggerJSON = require('./api-documentation/swagger.json')
+const swaggerUI = require('swagger-ui-express')
 
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+app.use('/docs', swaggerUI.serve, swaggerUI.setup(swaggerJSON))
 app.use('/api', router, express.static('public'))
 
 app.get("/", (req, res) => {
